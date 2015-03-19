d9cbf068337d5b50b23d764c4a10e5c42ca6b0cf
GarryY <garry.yavnilovich@safenet-inc.com>
2015-03-18 20:21:52 -0700

    some upload/get + client fixes + test shell

--- ssfilesss-client.py
+++ ssfilesss-client.py
@@ -1,31 +1,86 @@
 #!/usr/bin/python
 
-import time
 import sys
-import argparse
-import simplejson
 import os
+import argparse
+import httplib2
+from datetime import datetime
+import simplejson
 
 file_actions = ['upload', 'get']
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--action', choices=file_actions)
+ap.add_argument('--server', required=True)
+ap.add_argument('--action', choices=file_actions, required=True)
 ap.add_argument('--debug', action='store_const', dest='isDebug', const=True, default=False)
-ap.add_argument('--file')
+ap.add_argument('--path')
 rap = ap.parse_args()
 print rap
 
-if rap.action in file_actions and not rap.file:
-    print 'Please specify file for <', rap.action, '>'
+if rap.action in file_actions and not rap.path:
+    print 'Please specify path or link for <', rap.action, '>'
 
 if rap.isDebug:
     print 'Debug mode is ON'
 
+def success(resp, content):
+    return 'error' not in content
+
+def URI(action):
+    return 'http://%s:8080/%s' % (rap.server, action)
+
+def upload_file():
+    print 'Uploading file', rap.path, '...'
+    file_name = os.path.basename(rap.path)
+    rd = {'file': file_name, 'folder':None}
+    h = httplib2.Http()
+    resp, content = h.request(URI(rap.action),
+                              'POST',
+                              simplejson.dumps(rd),
+                              headers={'Content-Type': 'application/json'})
+    #TODO really - eval ??
+    print content
+    print 'Upload%s' % (' failed' if not success(resp, content) else 'ed')
+
+
+def get_file_content():
+    print 'Getting remote file content', rap.path, '...'
+    h = httplib2.Http()
+    resp, content = h.request(URI(rap.path),
+                              'GET',
+                              simplejson.dumps({}),
+                              headers={'Content-Type': 'application/json'})
+
+    print 'got:', content
+    if not success(resp, content):
+        print 'File content request failed'
+
+
+def upload_folder():
+    print 'Uploading folder', rap.path, '...'
+    raise NotImplementedError
 
 if __name__ == '__main__':
 
-    filename = __file__[:__file__.rfind('.')+1]
+    try:
+        if rap.action == 'upload':
+            if not os.path.exists(rap.path):
+                print 'The path does not exist:', rap.path
+                sys.exit(1)
 
+            if not os.path.isdir(rap.path):
+                upload_file()
+            else:
+                upload_folder()
+
+        elif rap.action == 'get':
+            get_file_content()
+        else:
+            print 'Unknown action <', rap.action, '>'
+
+    except Exception as e:
+        print 'Exception: ', repr(e)
+        sys.exit(1)
 
 
 
--- ssfilesss.py
+++ ssfilesss.py
@@ -4,13 +4,23 @@ from twisted.web import server, resource
 from twisted.internet import reactor, defer
 from configobj import ConfigObj
 import logging
-import time
+import commands
 import sys
 import argparse
 import simplejson
 import os
+import re
+
 
 LOG = logging.getLogger("ssfiless")
+
+
+class FileNotFoundException(Exception):
+    def __init__(self,fname=''):
+        self._fname = fname
+    def __str__(self):
+        return 'File not found: %s' % self._fname
+
 
 class SSFileServer(resource.Resource):
 
@@ -18,48 +28,77 @@ class SSFileServer(resource.Resource):
 
     def __init__(self):
         self.db = ConfigObj('ssfilesss.conf')
-        self.files_section = self.db['files']
+        self._files_section = self.db['files']
+        self._storage_location = os.path.abspath(self.db['general']['storage_location'])
+        if not os.path.exists(self._storage_location):
+            os.makedirs(self._storage_location)
 
-        self.storage_location = self.db['general']['storage_location']
-        if not os.path.exists(self.storage_location):
-            os.makedirs(self.storage_location)
+    #TODO - questionable...
+    def _uri(self, folder, fname):
+        ip = re.search( r'inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*eth0', commands.getoutput('ip a') ).groups()[0]
+        return 'http://%s:8080/%s' % (ip, os.path.join(folder, fname))
 
+    def _get_local_path(self, fname):
+        return os.path.join(self._storage_location, fname)
 
-    def getChild(self, name, request):
-        LOG.debug('getting child: %s' % name)
-        if not name:
-            return self
-        return resource.Resource.getChild(self, name, request)
+    def _file_exists(self, path):
+        return path in self._files_section and os.path.exists(path)
 
-    def _upload(self, request):
-        data = simplejson.loads(request.content.read())
-
-        LOG.debug('%s:handling request: %s', self.__class__.__name__, data)
-
-        upload_result = {'link': None}
-
-        #....................
-
-        result = simplejson.dumps({'result':upload_result})
-        return result
-
-    def render_POST(self, request):
-        LOG.debug('render_GET: uri=%s', request.uri)
-        d = defer.maybeDeferred(self._upload, request)
-        d.addCallback(self._upload_ready, request)
-        d.addErrback(self._upload_error, request)
-        return server.NOT_DONE_YET
-
-    def _upload_ready(self, result, request):
+    def _ready(self, result, request):
         LOG.debug('\tresult --> %s', result)
         request.write(result)
         request.finish()
 
-    def _upload_error(self, failure, request):
+    def _error(self, failure, request):
         LOG.debug('\t--> %s', failure)
         failure = {'error':str(failure.value)}
         request.write(simplejson.dumps(failure))
         request.finish()
+
+    ##################
+
+    def _upload(self, request):
+        data = simplejson.loads(request.content.read())
+
+        LOG.debug('Upload request %s', data)
+
+        folder = data['folder'] if 'folder' in data and data['folder'] else ''
+        upload_result = self._uri(folder, data['file'])
+
+        #....................
+
+        result = upload_result #simplejson.dumps(upload_result)
+        return result
+
+    def render_POST(self, request):
+        LOG.debug('render_POST: uri=%s', request.uri)
+        d = defer.maybeDeferred(self._upload, request)
+        d.addCallback(self._ready, request)
+        d.addErrback(self._error, request)
+        return server.NOT_DONE_YET
+
+    #################
+
+    def _get_content(self, request):
+        fname = os.path.basename(request.uri)
+        local_path = self._get_local_path(fname)
+        LOG.debug('File content request %s', local_path)
+
+        ###TODO check DB first !!!!!
+        if not self._file_exists(local_path):
+            request.setResponseCode(404)
+            raise FileNotFoundException(fname)
+        result = 'XXX'
+        return result
+
+    def render_GET(self, request):
+        LOG.debug('render_GET: uri=%s', request.uri)
+        d = defer.maybeDeferred(self._get_content, request)
+        d.addCallback(self._ready, request)
+        d.addErrback(self._error, request)
+        return server.NOT_DONE_YET
+
+
 
 
 
@@ -70,9 +109,7 @@ if rap.isDebug:
     print 'Debug mode is ON'
 
 if __name__ == '__main__':
-
     filename = __file__[:__file__.rfind('.')+1]
-
     FMT = "%(asctime)s - %(name)-6s - %(levelname)-6s - [%(filename)s:%(lineno)d] - %(message)s"
     logging.basicConfig(filename=filename+'log',
                         level=logging.DEBUG if rap.isDebug else logging.INFO,
@@ -85,8 +122,6 @@ if __name__ == '__main__':
 
     LOG.info('Starting service...')
     root = SSFileServer()
-    #root.putChild('greedy', WBSRouteMsgGreedy())
-    #root.putChild('greedy2', WBSRouteMsgGreedy2())
     site = server.Site(root)
     reactor.listenTCP(8080,site)
     reactor.run()
